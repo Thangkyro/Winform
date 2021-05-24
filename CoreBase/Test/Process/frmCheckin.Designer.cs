4e8eb6b631ed5a06c920a608dd86caeaadc9ac94
Thangkyro <79921592+Thangkyro@users.noreply.github.com>
2021-05-24 23:43:45 +0700

    1
    commit checking.

--- CoreBase/Test/Process/frmCheckin.Designer.cs
+++ CoreBase/Test/Process/frmCheckin.Designer.cs
@@ -367,6 +367,7 @@
             this.btnAdd.TabIndex = 4;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // cb_B_ServiceName
             // 
