db5c7781825cd5f08c5b58e176c7521b038e1ee9
Thangkyro <thang.nguyen@dmspro.vn>
2021-10-29 15:20:51 +0700

    1

--- CoreBase/NailTablet/Program.cs
+++ CoreBase/NailTablet/Program.cs
@@ -26,17 +26,17 @@ namespace AusNail
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            //Updater u = new Updater();
-            ////u.StartMonitoring();
-            //if (u.LocalConfig != null)
-            //{
-            //    if (u.ZCheckUpdate())
-            //    {
-            //        System.Windows.Forms.MessageBox.Show("Has a new version. The program will perform an update before proceeding further.", "Infomation");
-            //        System.Diagnostics.Process.Start("Nail.Update.exe");
-            //        return;
-            //    }
-            //}
+            Updater u = new Updater();
+            //u.StartMonitoring();
+            if (u.LocalConfig != null)
+            {
+                if (u.ZCheckUpdate())
+                {
+                    System.Windows.Forms.MessageBox.Show("Has a new version. The program will perform an update before proceeding further.", "Infomation");
+                    System.Diagnostics.Process.Start("Nail.Update.exe");
+                    return;
+                }
+            }
 
             // Check database setting
             //try
